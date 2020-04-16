478434c1891bdd5b7aac1cf81ee3c9dea8af9eeb
haeungray <haeungray@gmail.com>
2020-04-16 23:58:35 +0900

    Add img upload route

---
+++ practice/img_upload.py
@@ -0,0 +1,12 @@
+import io
+
+def test_save_and_get_profile(api):
+    resp = api.post(
+        '/login',
+        data = json.dumps({'email':'haeungrace@gmail.com',
+        'password':'test'},
+        content_type = 'application/json')
+    )
+    resp_json = json.loads(resp.data.decode('utf-8'))
+    access_token = resp_json['access_token']
+    
